d1fd710b7bd02dc00d25934d1dde5494d5808cd4
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2022-10-17 02:12:26 -0300

    aside improved

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,38 +1,26 @@
-import { AppShell, Button, Navbar, Stack } from '@mantine/core'
+import { Button } from '@mantine/core'
 import Link from 'next/link'
 import { ReactNode } from 'react'
 
+import styles from './aside.module.scss'
+
 export default function Aside({ children, path }: { children: ReactNode; path: string }) {
   return (
-    <AppShell
-      padding='md'
-      styles={{
-        main: {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-          flexDirection: 'row'
-        }
-      }}
-      navbar={
-        <Navbar>
-          <Stack spacing='xl'>
-            <Link href='/username'>
-              <Button disabled={path === 'username'}>Username</Button>
-            </Link>
-            <Link href='/password'>
-              <Button disabled={path === 'password'}>Password</Button>
-            </Link>
-            <Link href='/compound-interest-with-contributions'>
-              <Button disabled={path === 'compound-interest-with-contributions'}>
-                Compound interest with contributions
-              </Button>
-            </Link>
-          </Stack>
-        </Navbar>
-      }
-    >
-      {children}
-    </AppShell>
+    <div className={styles.root}>
+      <aside className={styles.aside}>
+        <Link href='/username'>
+          <Button disabled={path === 'username'}>Username</Button>
+        </Link>
+        <Link href='/password'>
+          <Button disabled={path === 'password'}>Password</Button>
+        </Link>
+        <Link href='/compound-interest-with-contributions'>
+          <Button disabled={path === 'compound-interest-with-contributions'}>
+            Compound interest with contributions
+          </Button>
+        </Link>
+      </aside>
+      <main className={styles.main}>{children}</main>
+    </div>
   )
 }
