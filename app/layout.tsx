2580a29ffdd4412c2de38203f2120fa5a6c365b3
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2022-12-23 02:10:34 -0300

    root layout modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,34 @@
+'use client'
+
+import { Button } from '@mantine/core'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import styles from './layout.module.scss'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  const path = pathname?.split('/')[1]
+
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={styles.root}>
+        <aside className={styles.aside}>
+          <Link href='/username'>
+            <Button disabled={path === 'username'}>Username</Button>
+          </Link>
+          <Link href='/password'>
+            <Button disabled={path === 'password'}>Password</Button>
+          </Link>
+          <Link href='/compound-interest-with-contributions'>
+            <Button disabled={path === 'compound-interest-with-contributions'}>
+              Compound interest with contributions
+            </Button>
+          </Link>
+        </aside>
+        <main className={styles.main}>{children}</main>
+      </body>
     </html>
   )
 }
