1458d550599be552ab7791cf7cb294a5c868ff5d
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2022-12-25 14:30:13 -0300

    overall styles improved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,38 @@
 'use client'
 
 import './globals.scss'
+import { Inter } from '@next/font/google'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { ReactNode } from 'react'
 
 import styles from './layout.module.scss'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname()
 
-  const path = pathname?.split('/')[1]
+  const path = pathname!.split('/')[1]
 
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.className}>
       <body className={styles.body}>
         <aside className={styles.aside}>
-          <h2>Generators</h2>
+          <h2 className={styles.heading}>Generators</h2>
           <Link href='/username'>
-            <button disabled={path === 'username'}>Username </button>
+            <button className={styles.button} disabled={path === 'username'}>
+              Username
+            </button>
           </Link>
           <Link href='/password'>
-            <button disabled={path === 'password'}> Password </button>
+            <button className={styles.button} disabled={path === 'password'}>
+              Password
+            </button>
           </Link>
-          <h2>Calculators</h2>
+          <h2 className={styles.heading}>Calculators</h2>
           <Link href='/compound-interest-with-contributions'>
-            <button disabled={path === 'compound-interest-with-contributions'}>
+            <button className={styles.button} disabled={path === 'compound-interest-with-contributions'}>
               Compound interest with contributions
             </button>
           </Link>
